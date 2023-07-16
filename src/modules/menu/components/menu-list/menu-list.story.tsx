7fe78f1b93b8445dbf98dac118a19128b0bd2439
Sin-god <illya.komarnickii1@gmail.com>
2023-07-16 13:31:45 +0300

    Add props to MenuList

--- src/modules/menu/components/menu-list/menu-list.story.tsx
+++ src/modules/menu/components/menu-list/menu-list.story.tsx
@@ -1,15 +1,18 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { MenuList } from "./menu-list.component";
+import pizzaMenu from "@app/mocks/pizza.json";
 
 const meta: Meta<typeof MenuList> = {
   title: "Menu/Menu List",
   component: MenuList,
-  args: {},
+  args: {
+    items: pizzaMenu,
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof MenuList>;
 
 export const Primary: Story = {
-  render: () => <MenuList />,
+  render: (args) => <MenuList {...args} />,
 };
---
+++ src/modules/menu/types/pizza.ts
@@ -0,0 +1,8 @@
+export interface Pizza {
+  id: null;
+  image: string;
+  weight: number;
+  title: string;
+  ingredients: string;
+  price: number;
+}
