081fff6946155d13e5baa368fe3a60f287a23044
Sin-god <illya.komarnickii1@gmail.com>
2023-07-18 18:53:38 +0300

    Add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { Footer } from "./common/commonts/footer/footer.components";
+import { Header } from "./common/commonts/header/header.component";
+import { MenuList } from "./modules/menu/components/menu-list/menu-list.component";
+import pizzaMenu from "@app/mocks/pizza.json";
+
 export const App = () => {
-  return <h1 className="font-bold text-3xl text-red-500">PizzaStack</h1>;
+  return (
+    <div>
+      <Header />
+      <div className="mx-12 mb-24">
+        <MenuList items={pizzaMenu} />
+      </div>
+      <Footer />
+    </div>
+  );
 };
